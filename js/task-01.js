9c0a69c3eb02bbfbe593a3bf575b1b53344e8647
Alexandr Nechuhaev <a.nechuhaev@gmail.com>
2022-08-04 19:29:57 +0300

    Task1-done

--- js/task-01.js
+++ js/task-01.js
@@ -1,8 +1,7 @@
 const listCategory = document.querySelectorAll('.item');
 console.log(`Number of categories:`, listCategory.length);
 
-const categoryName = document.querySelector('h2');
-console.log(`Category:`, categoryName.textContent);
-
-const categoryEl = document.querySelectorAll('li li.item');
-console.log(`Elements:`, categoryEl.length);
+listCategory.forEach((item) => {
+    console.log(`Category:, ${item.firstElementChild.textContent}`);
+    console.log(`Elements:, ${item.lastElementChild.children.length}`);
+ });
