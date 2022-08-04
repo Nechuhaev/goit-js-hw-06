14b2f0d3ddb1eb11f9659df7c1da364cdbecf6a1
Alexandr Nechuhaev <a.nechuhaev@gmail.com>
2022-08-04 20:21:14 +0300

    Task2-start

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,12 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+const ingredientsLi = document.createElement(`li`);
+ingredientsLi.classList.add(`item`);
+ingredientsLi.textContent = `Potatos`;
+console.log(ingredientsLi);
+
+document.body.appendChild(ingredientsLi);
+
+console.log(document.ingredients);
