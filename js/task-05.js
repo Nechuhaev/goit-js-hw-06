4cf410c6dde4d5ed3994832584a301c4ba930f69
Alexandr Nechuhaev <a.nechuhaev@gmail.com>
2022-08-06 16:19:15 +0300

    task-5 done

--- js/task-05.js
+++ js/task-05.js
@@ -2,5 +2,8 @@ const textInput = document.querySelector("#name-input");
 const textOutput = document.querySelector("#name-output");
 
 textInput.addEventListener("input", (event) => {
-  textOutput.textContent = event.currentTarget.value;
+    textOutput.textContent = event.currentTarget.value;
+    if (event.currentTarget.value === "") {
+        textOutput.textContent = 'Anonymous'
+    }
 });
